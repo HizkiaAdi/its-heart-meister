e81ae467d2b4367fc057f3114e69a4ec4841103f
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-21 12:17:31 +0000

    ngubah2 lagi

---
+++ Heart Meister/Assets/SocialModule/Scripts/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace SocialModule
+{
+	public class PlayerManager : MonoBehaviour
+	{
+		private List<GameObject> players;
+		private List<Player> playerDatas;
+		
+		void Start()
+		{
+			this.players = new List<GameObject>();
+			this.playerDatas = new List<Player>();
+		}
+		
+		public void NewMessage(string jsonString)
+		{
+			Debug.Log("message from server: " + jsonString);
+		}
+		
+		void Update()
+		{
+			foreach(Player p in playerDatas)
+			{
+				Vector2 position = p.Position;
+				Vector2 vector = p.Vector;
+				position.x = vector.x * Time.deltaTime;
+				position.y = vector.y * Time.deltaTime;
+				p.Position = position;
+				p.Vector = vector;
+			}
+		}
+		
+		private void AddPlayer()
+		{
+			
+		}
+		
+		private void RemovePlayer()
+		{
+			
+		}
+		
+		void FixedUpdate()
+		{
+			
+		}
+	}
+
+}
