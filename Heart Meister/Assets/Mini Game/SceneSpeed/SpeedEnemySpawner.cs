dcaab02e08917ebdbdc7885f40038b5e5c3477ec
afrizal.kudo@gmail.com <afrizal.kudo@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-21 02:57:29 +0000

    nambah mini game speed

---
+++ Heart Meister/Assets/Mini Game/SceneSpeed/SpeedEnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedEnemySpawner : MonoBehaviour {
+
+    public GameObject[] enemy;
+    float spawnDelay = 1.5f;
+    float nextSpawn = 0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnDelay;
+            int maxSpawn = Random.Range(1, 3);
+            for (int i = 0; i < maxSpawn; i++)
+            {
+                int rand = Random.Range(0, 7);
+                if (rand != 5)
+                    Instantiate(enemy[0], new Vector2(transform.position.x, transform.position.y + Random.Range(-3f, 3f)), transform.rotation);
+                else Instantiate(enemy[1], new Vector2(transform.position.x, transform.position.y + Random.Range(-3f, 3f)), transform.rotation);
+            }
+        }
+    }
+}
