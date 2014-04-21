0b9acfce1843283d107b214ec8573335d5984605
afrizal.kudo@gmail.com <afrizal.kudo@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-21 02:57:29 +0000

    nambah mini game speed

---
+++ trunk/Heart Meister/Assets/Mini Game/SceneSpeed/SpeedGameManager.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedGameManager : MonoBehaviour {
+
+    public GUIText completeText, failedText, chanceText, scoreText;
+    public static int score, chance;
+    bool isComplete, isGameOver;
+    // Use this for initialization
+    void Start()
+    {
+        completeText.enabled = false;
+        failedText.enabled = false;
+        score = 0;
+        chance = 3;
+        isComplete = false;
+        isGameOver = false;
+
+        GameEventManager.GameOver += GameOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score < 10)
+        {
+            scoreText.text = score.ToString() + "/10";
+        }
+        else
+        {
+            scoreText.text = "10/10";
+            isComplete = true;
+        }
+
+        if (chance > 0)
+            chanceText.text = "Chance: " + chance.ToString();
+        else chanceText.text = "Chance: 0";
+
+        if (score == 10 || chance == 0)
+        {
+            isGameOver = true;
+            GameEventManager.TriggerGameOver();
+        }
+
+        RaycastHit2D tapEnemy = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (Input.GetMouseButtonUp(0) && tapEnemy.collider != null)
+        {
+            if (tapEnemy.collider.tag == "Enemy")
+            {
+                Destroy(tapEnemy.collider.gameObject);
+                score++;
+            }
+
+            else if (tapEnemy.collider.tag == "OtherObject")
+            {
+                Destroy(tapEnemy.collider.gameObject);
+                chance--;
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        if (isComplete)
+        {
+            completeText.enabled = true;
+        }
+        else
+        {
+            failedText.enabled = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isGameOver)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 20, Screen.height / 2 + 20, 40, 30), "OK"))
+            {
+                Application.LoadLevel(0);
+            }
+        }
+    }
+}
---
+++ trunk/Heart Meister/Assets/Mini Game/SceneSpeed/SpeedOtherObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedOtherObject : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - 0.08f, transform.position.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Boundary")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
