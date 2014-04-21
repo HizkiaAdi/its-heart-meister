f681ee25e8fa2ae9f2141cfc2ebb86d95c354fdc
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-21 12:17:31 +0000

    ngubah2 lagi

---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace SocialModule
+{
+	public class Player
+	{
+		private string id;
+		private string name;
+		private Vector2 vector;
+		private Vector2 position;
+		private int state;
+		
+		public Player()
+		{
+			this.id = "placeholder";
+			this.name = "name";
+			this.vector = Vector2.zero;
+			this.position = new Vector2(-0, -100);
+		}
+		
+		public Player(string id, string name, Vector2 vector, Vector2 position, int state)
+		{
+			this.id = id;
+			this.name = name;
+			this.vector = vector;
+			this.position = position;
+			this.state = state;
+		}
+		
+		public string ID {get{return this.id;} set{this.id = value;}}
+		
+		public string Name {get{return this.name;} set{this.name = value;}}
+		
+		public Vector2 Vector {get{return this.vector;} set{this.vector = value;}}
+		
+		public Vector2 Position {get{return this.position;} set{this.position = value;}}
+		
+		public int State {get{return this.state;} set{this.state = value;}}
+	}
+}
---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour
+{
+	public static string id = "5110100082";
+	public static string playerName = "Hizkia";
+	
+	public GUISkin guiSkin;
+	
+	void OnGUI()
+	{
+		GUI.skin = guiSkin;
+		GUI.Label(new Rect(25, 10, 50, 20), "ID");
+		id = GUI.TextField(new Rect(135, 10, 150, 20), id);
+		GUI.Label(new Rect(25, 40, 50, 20), "Name");
+		playerName = GUI.TextField(new Rect(135, 40, 150, 20), playerName);
+	}
+}
