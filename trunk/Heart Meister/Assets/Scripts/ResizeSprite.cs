4ef3dce9155fd47e2a9b85ff9369612404ee084a
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-22 07:26:09 +0000

    Update

--- trunk/Heart Meister/Assets/Scripts/ResizeSprite.cs
+++ trunk/Heart Meister/Assets/Scripts/ResizeSprite.cs
@@ -18,13 +18,9 @@ public class ResizeSprite : MonoBehaviour
 
 		float width = sr.sprite.bounds.size.x;
 		float height = sr.sprite.bounds.size.y;
-		Debug.Log ("SpriteW: " + width);
-		Debug.Log ("SpriteH: " + height);
 
 		float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
 		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
-		Debug.Log ("WScreenW: " + worldScreenWidth);
-		Debug.Log ("WScreenH: " + worldScreenHeight);
 
 		transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
 	}
---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/PlayerSocketListener.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace SocialModule
+{
+	public class PlayerSocketListener : MonoBehaviour 
+	{
+		public string host;
+		public int port;
+		
+		private AndroidJavaClass _unityPlayer;
+		private AndroidJavaObject _activity;
+		private AndroidJavaObject _playerClient;
+		
+		void Start ()
+		{
+			string[] args = new string[2];
+			args[0] = host;
+			args[1] = port.ToString();
+			
+			_unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			_activity = _unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+			_activity.Call("runOnUiThread", new AndroidJavaRunnable(()=>
+        	{
+				_playerClient = new AndroidJavaObject("com.pejuangcinta.player.PlayerClient", args);
+			}
+			));
+		}
+		
+		public void ReceivedMessage(string message)
+		{
+			GameObject.Find("PlayerListener").GetComponent<PlayerManager>().NewMessage(message);
+		}
+		
+		void FixedUpdate ()
+		{
+			bool success = false;
+			string jsonString = PlayerDataToJSON(transform);
+			success = _playerClient.Call<bool>("sendMessage", jsonString);
+			if (success)
+				Debug.Log("Successfully sent message.");
+			else
+				Debug.Log("Failed to send message.");
+		}
+		
+		private string PlayerDataToJSON(Transform transform)
+		{
+			Dictionary<string,string> dict = new Dictionary<string, string>();
+			dict["x"] = transform.localPosition.x.ToString();
+			dict["y"] = transform.localPosition.y.ToString();
+			dict["id"] = PlayerData.id;
+			dict["name"] = PlayerData.playerName;
+			dict["state"] = "0";
+			return MiniJSON.Json.Serialize(dict);
+		}
+	}
+}
