a25e76834964fa54447089ad622e8bbb6878f634
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-22 04:08:27 +0000

    update :)

---
+++ Heart Meister/Assets/Scripts/ResizeSprite.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeSprite : MonoBehaviour
+{
+	void Start () 
+	{
+		ResizeSpriteToScreen();
+	}
+
+	void ResizeSpriteToScreen()
+	{
+		SpriteRenderer sr = this.gameObject.GetComponent<SpriteRenderer>();
+		if (sr == null)
+			return;
+
+		transform.localScale = new Vector3(1, 1, 1);
+
+		float width = sr.sprite.bounds.size.x;
+		float height = sr.sprite.bounds.size.y;
+		Debug.Log ("SpriteW: " + width);
+		Debug.Log ("SpriteH: " + height);
+
+		float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		Debug.Log ("WScreenW: " + worldScreenWidth);
+		Debug.Log ("WScreenH: " + worldScreenHeight);
+
+		transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+	}
+}
