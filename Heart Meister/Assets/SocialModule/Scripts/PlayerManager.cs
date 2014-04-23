8eced6936affce92a6886e0e2a11c488a06f2dc0
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-23 15:39:35 +0000

    update

--- Heart Meister/Assets/SocialModule/Scripts/PlayerManager.cs
+++ Heart Meister/Assets/SocialModule/Scripts/PlayerManager.cs
@@ -16,6 +16,8 @@ namespace SocialModule
 			players = new Dictionary<string, Player> ();
 			playersKey = new List<string> ();
 		}
+
+		void Update()
 		
 		public void NewMessage(string jsonString)
 		{
@@ -24,20 +26,6 @@ namespace SocialModule
 			Dictionary<string, System.Object> tempDict;
 			Player player;
 
-			//resultList = (List<System.Object>)Json.Deserialize(jsonString);
-
-			/*
-			//New Player
-			tempDict = (Dictionary<string, System.Object>)resultList [0];
-			tempList = (List<System.Object>)tempDict ["newplayer"];
-			foreach (System.Object o in tempList)
-			{
-				tempDict = (Dictionary<string, System.Object>) o;
-				AddPlayer();
-			}
-			*/
-
-			//PlayerData
 			tempDict = (Dictionary<string, System.Object>)Json.Deserialize(jsonString);
 			tempList = (List<System.Object>)tempDict["data"];
 			foreach (System.Object o in tempList)
@@ -54,19 +42,19 @@ namespace SocialModule
 					player = new Player(tempDict["id"].ToString(), tempDict["name"].ToString(),
 					                    new Vector2(float.Parse(tempDict["vectorX"].ToString()), float.Parse(tempDict["vectorY"].ToString())),
 					                    new Vector2(float.Parse(tempDict["x"].ToString()), float.Parse(tempDict["y"].ToString())),
-					                    0, AddPlayer());
+					                    0);
 					players.Add((string)tempDict["id"], player);
-					AddPlayer();
+					AddPlayer(player);
 				}
 			}
 		}
 
-		private GameObject AddPlayer()
+		private void AddPlayer(Player player)
 		{
 			Debug.Log("Creating game object");
 			GameObject obj = (GameObject)Instantiate (playerObject, this.transform.position, Quaternion.identity);
-			Debug.Log(obj);
-			return obj;
+			player.GameObject = obj;
+			obj.GetComponent<NPCController>().SetPlayerData = player;
 		}
 		
 		private void RemovePlayer()
