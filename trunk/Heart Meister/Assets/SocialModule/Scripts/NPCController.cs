d98198544523a6530a8dc97f5337e35ef8e190e8
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-24 01:48:54 +0000

    update

---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/NPCController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SocialModule
+{
+	public class NPCController : MonoBehaviour 
+	{
+		Player player;
+		Vector2 position;
+
+		void Start () 
+		{
+			
+		}
+		
+		
+		void Update () 
+		{
+			if(player != null)
+			{
+				this.transform.position = player.Position;
+			}
+		}
+
+		public Player SetPlayerData
+		{
+			set{this.player = value;}
+		}
+	}
+
+}
