b949ac5166bb7ae01a0425c5716bba29e3a588e2
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-04-28 08:36:23 +0000

    Updat prototype modul 4

--- trunk/Heart Meister/Assets/CustomGUI/Scripts/CustomGUI/GUITextField.cs
+++ trunk/Heart Meister/Assets/CustomGUI/Scripts/CustomGUI/GUITextField.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CustomGUI
 {
-	public class GUIButton : IGUIElement
+	public class GUITextField : IGUIElement
 	{
 		private int id;
 		private Rect rect;
@@ -11,22 +11,31 @@ namespace CustomGUI
 		private float yUnit;
 		private string text;
 		private GUICallback callback;
-
-		public GUIButton(int id, float x, float y, float width, float height, string text, GUICallback callback)
+		private Dictionary<string, string> callbackRes;
+		
+		public GUITextField(int id, float x, float y, float width, float height, string defaultVal, GUICallback callback)
 		{
 			this.id = id;
 			this.xUnit = Screen.width * 0.01f;
 			this.yUnit = Screen.height * 0.01f;
 			this.rect = new Rect(x * xUnit, y * yUnit, width * xUnit, height * yUnit);
-			this.text = text;
+			this.text = defaultVal;
 			this.callback = callback;
+			this.callbackRes = new Dictionary<string, string>();
 			UnityEngine.GameObject.Find("GUIContainner").GetComponent<GUIContainner>().AddGUIElement(this);
 		}
-
+		
 		public void Draw()
 		{
-			if(UnityEngine.GUI.Button(this.rect, this.text))
-				this.callback(this.id, null);
+			this.text = UnityEngine.GUI.TextField(this.rect, this.text);
+			if(callbackRes.ContainsKey("text"))
+			{
+				callbackRes["text"] = this.text;
+			}
+			else
+				callbackRes.Add("text", this.text);
+			this.callback(this.id, this.callbackRes);
 		}
 	}
+
 }
