f3a34fbfba3a61b0c2521e3dce6da2fb26d1528e
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-05-05 13:12:33 +0000

    Update Avatar dll

---
+++ Heart Meister/Assets/SocialModule/Scripts/Avatar/AvatarIconScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SocialModule.Avatar
+{
+	public class AvatarIconScript : MonoBehaviour
+	{
+		public IAvatar avatarData;
+		//public Transform selected;
+
+		/*void OnTriggerEnter(Collider other)
+		{
+			if(other.CompareTag("HeadAvatar")||
+			   other.CompareTag("TopAvatar")||
+			   other.CompareTag("BottomAvatar"))
+			{
+				this.selected = other.gameObject.transform;
+			}
+		}*/	
+	}
+}
--- Heart Meister/Assets/SocialModule/Scripts/Avatar/HeadAvatar.cs
+++ Heart Meister/Assets/SocialModule/Scripts/Avatar/HeadAvatar.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace SocialModule.Avatar
 {
-    public class HeadAvatar : Avatar
+    public class HeadAvatar : Avatars
     {
 		public HeadAvatar(Sprite sprite, Sprite editorSprite, Sprite iconSprite, int id, string avatarName)
             : base(sprite, editorSprite, iconSprite, id, avatarName)
