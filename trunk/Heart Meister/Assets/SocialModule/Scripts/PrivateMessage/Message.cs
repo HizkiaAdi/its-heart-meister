b79180bef00b5d26dd1027767cb02a25092463d9
hizkia92@gmail.com <hizkia92@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-05-08 15:12:09 +0000

    update PrivateMessage

---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/PrivateMessage/Message.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Message
+{
+	public int Id { get; set; }
+	public int SenderId { get; set; }
+	public string SenderName { get; set; }
+	public string MessageString { get; set; }
+	public string Date { get; set; }
+
+	public Message(int id, int senderid, string sendername, string messagestring, string date)
+	{
+		this.Id = id;
+		this.SenderId = senderid;
+		this.SenderName = sendername;
+		this.MessageString = messagestring;
+		this.Date = date;
+	}
+
+	public override string ToString ()
+	{
+		return string.Format ("[Message: Id={0}, " +
+			"SenderId={1}, " +
+			"SenderName={2}, " +
+			"MessageString={3}, " + "" +
+      		"Date={4}]", 
+      		Id, SenderId, SenderName, MessageString, Date);
+	}
+}
---
+++ trunk/Heart Meister/Assets/SocialModule/Scripts/PrivateMessage/MessageChecker.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MessageChecker : MonoBehaviour 
+{
+	int i;
+	List<Message> messageList;
+
+	void Awake ()
+	{
+		messageList = new List<Message>();
+	}
+
+	void Start () 
+	{
+		/*
+		 * To-do:
+		 * Get all message from webservice
+		 */
+		for(i = 0 ; i < 50; i++)
+			messageList.Add(new Message(i,1,"Hizkia","Haha " + i, System.DateTime.Now.ToString()));
+		
+		/*foreach(Message i in messageList)
+			Debug.Log(i.ToString());*/
+
+		InvokeRepeating("CheckForNewMessage", 0, 3);
+	}
+
+	void CheckForNewMessage()
+	{
+		/*
+		To-do:
+		Check new message from webservice
+		*/
+		//messageList.Add(new Message(++i,1,"Hizkia","Haha " + i, System.DateTime.Now.ToString()));
+	}
+
+	public void GetMessageList(out List<Message> param)
+	{
+		param = this.messageList;
+	}
+}
