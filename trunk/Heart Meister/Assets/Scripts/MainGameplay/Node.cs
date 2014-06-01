50bc2619a6cc7bb2faff6820f4f22971197b3a19
remy.mangowal@gmail.com <remy.mangowal@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-06-01 13:54:48 +0000

    wtf

---
+++ trunk/Heart Meister/Assets/Scripts/MainGameplay/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace MainGameplay
+{
+    public class Node
+    {
+        public string nodeName;
+        public int nodeType;
+        public float nodeLocX;
+        public float nodeLocY;
+        public int nodeID;
+        public List<int> nodeChild;
+        //type = 0 is not a passable node, 1 is start node, 2 is idle node, 3 is battle node, 4 is boss node, 5 is event node
+
+        public Node(string name, int type, float locX, float locY, int id, List<int> child)
+        {
+            this.nodeName = name;
+            this.nodeType = type;
+            this.nodeLocX = locX;
+            this.nodeLocY = locY;
+            this.nodeID = id;
+            this.nodeChild = child;
+        }
+    }
+}
