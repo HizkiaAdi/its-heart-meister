604ca3259a59a6998e4a414213e604fd27e273fb
remy.mangowal@gmail.com <remy.mangowal@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-06-23 11:58:06 +0000

    sblm lupa p3

---
+++ Heart Meister/Assets/Scripts/MainGameplay/RaeyLibrary/ScreenTransitionManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace MainGameplay
+{
+
+    public class ScreenTransitionManager : MonoBehaviour
+    {
+        public LoaderBehavior lb;
+        public static string targetName;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+        void GoLoader(string target)
+        {
+            targetName = target;
+            Application.LoadLevel("LoadScreen");
+        }
+    }
+}
