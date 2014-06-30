48421b99373452c415d374e8e9e937b08cde8caa
remy.mangowal@gmail.com <remy.mangowal@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-06-30 15:30:31 +0000

    fixing

---
+++ Heart Meister/Assets/Scripts/MainGameplay/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public GameObject target;
+    public string functionName;
+    public string functionParam;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        var audio = GetComponent<AudioSource>();
+        audio.enabled = true;
+        audio.Play();
+
+        HOTween.To(transform, 0.2f, new TweenParms().Prop("localScale", new Vector3(0.5f, 0.5f)).OnComplete(target, functionName, functionParam, SendMessageOptions.DontRequireReceiver));
+        HOTween.To(transform, 0.2f, new TweenParms().Prop("localScale", new Vector3(1f, 1f)).Delay(0.2f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
