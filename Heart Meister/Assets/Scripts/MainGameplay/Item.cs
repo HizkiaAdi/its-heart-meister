6e219cefe0177eb837cf264952a4f403d78c784f
remy.mangowal@gmail.com <remy.mangowal@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-07-20 14:54:27 +0000

    wtv

---
+++ Heart Meister/Assets/Scripts/MainGameplay/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace MainGameplay
+{
+
+    public class Item
+    {
+        public string itemID;
+        public string itemName;
+        public int itemCategory;
+        public int itemQuantity;
+        public int itemBuyPrice;
+        public int itemSellPrice;
+    }
+}
