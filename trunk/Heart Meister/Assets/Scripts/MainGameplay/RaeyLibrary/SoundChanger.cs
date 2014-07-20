ca0269f9dba2c66b167ab9767f02ffa38982a293
remy.mangowal@gmail.com <remy.mangowal@gmail.com@1b446fa8-285b-7de4-7e86-c9f5d652bb23>
2014-07-20 14:53:40 +0000

    test karena stress

---
+++ trunk/Heart Meister/Assets/Scripts/MainGameplay/RaeyLibrary/SoundChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundChanger : MonoBehaviour {
+
+    public AudioClip NewMusic;
+
+    void Awake()
+    {
+        GameObject go = GameObject.Find("RaeySoundManager");
+        go.audio.clip = NewMusic;
+        go.audio.Play();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
